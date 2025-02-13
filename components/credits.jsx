94a729b2f41c0e2f88fe99cb4894226f402f03f9
Sandy Aprilyanto <128687467+Intexcloud@users.noreply.github.com>
2025-02-13 16:58:43 +0700

    Update credits.jsx

--- components/credits.jsx
+++ components/credits.jsx
@@ -5,9 +5,7 @@ const Credits = (props) => {
         <div data-section id='credits' className='group mt-32'>
             <div className='text-surface-600'>
                 <div className='flex flex-row justify-between'>
-                    <a href="/imprint" rel="noopener noreferrer" className="scroll-auto transition-all hover:text-on-background">Imprint</a>
-                    <a href="/privacy" rel="noopener noreferrer" className="scroll-auto transition-all hover:text-on-background">Privacy Statement</a>
-                    <span>{props.data.name} | 2023</span>
+                    <span>{props.data.name} | 2025</span>
                 </div>
             </div>
         </div>
